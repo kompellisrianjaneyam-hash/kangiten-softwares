edbb43d5b8b0d5da9f5c27a456af98bef6bbc1a8
SRI ANJANEYAM <srianjaneyam@MacBookAir.lan>
2026-06-10 21:40:32 +0530

    Add robots file

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,7 +5,14 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: "https://kangitensoftwares.in",
       lastModified: new Date(),
+      changeFrequency: "weekly",
       priority: 1,
+    },
+    {
+      url: "https://kangitensoftwares.in/services",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
     },
   ];
 }
