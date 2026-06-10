ff008a17a5859dacd8b9d89448aae8a59983a6ff
SRI ANJANEYAM <srianjaneyam@MacBookAir.lan>
2026-06-10 21:07:17 +0530

    Fix sitemap domain

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://kangitensoftware.com",
+      url: "https://kangitensoftwares.in",
       lastModified: new Date(),
       priority: 1,
     },
